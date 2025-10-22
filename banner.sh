0fbdce73df4974091171627e05fce21a86c0ad9c
Mattia Babbini <mattiababbini99@gmal.com>
2025-10-22 19:29:35 +0200

    refactor(banner.sh) switch case -> arrays
    Refactor del codice (disgustoso) che avevo scritto
    - invece di usare uno switch case uso array e prendo l'indice casuale
    dell'array
    Più veloce e futureproof

--- banner.sh
+++ banner.sh
@@ -2,64 +2,28 @@
 
 
 random_font(){
-  banner=$1
-  font="chunky"
-  case "$banner" in
-    1) font="Chunky"
-    ;;
-    2) font="Letters"
-    ;;
-    3) font="smshadow"
-    ;;
-    4) font="script"
-    ;;
-    5) font="mini"
-    ;;
-    6) font="bubble"
-    ;;
-    7) font="digital"
-    ;;
-    8) font="banner" 
-    ;;
-    *) font="Isometric2" 
-    ;;
-  esac
-  figlet -f $font -c "CaffeLatte" | lolcat
-
+  font_list=("Chunky" "Letters" "smshadow" "script" "mini" "bubble" "digital" "banner" "Isometric2")
+  random=$((RANDOM % ${#font_list[@]}))
+  figlet -f ${font_list[$random]} "CaffeLatte" | lolcat
 }
 one_liner(){
-  case "$1" in 
-    1) word="Why don't jokes work in octal? Because 7 10 11."
-    ;;
-    2) word="I could tell you a joke about UDP but I don't know if you would get it."
-    ;;
-    3) word="There are only two difficult problems in computer science: naming things, cache invalidation, and off-by-one errors."
-    ;;
-    4) word="You just have to git gud."
-    ;;
-    5) word=".titanic { float:none;}"
-    ;;
-    6) word="There are two major products of Berkeley, CA -- LSD and UNIX. We don't believe this to be strictly by coincidence."
-    ;;
-    7) word="May the source be with you."
-    ;;
-    8) word="There is no place like ~"
-    ;;
-    9) word="One of my most productive days was throwing away 1000 lines of code.\nKen Thomson"
-    ;;
-    10) word="Hackers do it with brute force."
-    ;;
-    11) word="The \"S\" in \"IoT\" stands for security."
-    ;;
-    *)
-    ;;
-   esac
-    printf "\n$word\n" | lolcat 
+  jokes=(
+    "Why don't jokes work in octal? Because 7 10 11." 
+    "I could tell you a joke about UDP but I don't know if you would get it."
+    "There are only two difficult problems in computer science: naming things, cache invalidation, and off-by-one errors."
+    "You just have to git gud."
+    ".titanic { float:none;}"
+    "There are two major products of Berkeley, CA -- LSD and UNIX. We don't believe this to be strictly by coincidence."
+    "May the source be with you."
+    "There is no place like ~"
+    "One of my most productive days was throwing away 1000 lines of code.\nKen Thomson"
+    "Hackers do it with brute force."
+   )
+   random=$((RANDOM % ${#jokes[@]}))
+   printf "\n${jokes[$random]}\n" | lolcat 
 }
 
 
-n1=$((RANDOM % 8 +1))
-n2=$((RANDOM % 11 +1 ))
-random_font "$n1"
-one_liner "$n2"
+random_font $n1
+one_liner $n2
 

21745b957403e7a50ee40d5ac577ebab38f5f6f9
CaffeLatteIV <mattiababbini99@gmail.com>
2025-11-25 14:50:56 +0100

    feat: keymaps to switch between open buffers

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 vim.keymap.set("n", "<leader>f", function()
 	require("conform").format()
 end, { desc = "Format buffer" })
+
+-- Navigating buffers
+vim.keymap.set("n", "<leader>bb", "<C-^>", { desc = "Switch to alternate buffer" })
+vim.keymap.set("n", "<leader>bn", ":bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<cr>", { desc = "Previous buffer" })

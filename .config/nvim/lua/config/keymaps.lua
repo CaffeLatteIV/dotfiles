c2c4f6bb8fc14d46907ce13160ef364f0a05d0bf
CaffeLatteIV <mattiababbini99@gmail.com>
2025-11-26 18:23:04 +0100

    feat: swtich workspaces, toggle visible whitespace chars

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 vim.keymap.set("n", "<leader>f", function()
 	require("conform").format()
 end, { desc = "Format buffer" })
+
+-- Navigating buffers
+vim.keymap.set("n", "<leader>bb", "<C-^>", { desc = "Switch to alternate buffer" })
+vim.keymap.set("n", "<leader>.", ":bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>,", ":bprevious<cr>", { desc = "Previous buffer" })
+
+-- Toggle visible whitespace characters
+vim.keymap.set("n", "<leader>l", ":listchars!<cr>", { desc = "Toggle [l]istchars" })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })

4a887e381c6eca7f97e0fde3a14f7b1f80c48f41
CaffeLatteIV <mattiababbini99@gmail.com>
2026-06-26 10:51:26 +0200

    fix: alcune volte crashava, ora ho messo un controllo che non lo fa piu' fare

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,11 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
+		-- pcall (Protected Call) catches the error so Neovim doesn't crash
+		local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+		if not status_ok then
+			return -- Silently exit so Lazy can finish booting and download the files!
+		end
 
 		configs.setup({
 			-- Automatically install parsers for markdown and html
